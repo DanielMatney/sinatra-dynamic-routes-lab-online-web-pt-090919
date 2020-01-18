6bf9f5c84a81f18d247b79c5aa43e2a710baac40
Daniel Matney <danieljmatney@gmail.com>
2020-01-18 22:31:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,7 @@ class App < Sinatra::Base
   
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
-    @number.times do
-      @string += params[:phrase]  
+    
     end
     @string
   end
